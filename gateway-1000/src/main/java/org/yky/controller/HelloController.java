8eacfa70e91eef460fa9f34b6bfc3ef10107c66b
ykycoder <649074096@qq.com>
2025-01-05 11:37:01 +0800

    实现微服务网关鉴权

--- gateway-1000/src/main/java/org/yky/controller/HelloController.java
+++ gateway-1000/src/main/java/org/yky/controller/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/m")
+@RequestMapping("/g")
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello world~";
+        return "Hello gateway~";
     }
 }
---
+++ mychat-common/src/main/java/org/yky/constant/UserConstant.java
@@ -0,0 +1,17 @@
+package org.yky.constant;
+
+public interface UserConstant {
+
+    public static final String GENDER_MALE = "0";
+
+    public static final String GENDER_FEMALE = "1";
+
+    public static final String GENDER_UNKNOWN = "2";
+
+    public static final String DEFAULT_BIRTH = "1999-10-01";
+
+    public static final String DEFAULT_NICK = "萌新";
+
+    public static final String USER_FOLLOWING_GROUP_TYPE_DEFAULT = "2";
+}
+
